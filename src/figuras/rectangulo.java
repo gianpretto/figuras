2e87941ccc83454aa2002a69b75223d60136f53b
unknown <gianpretto@hotmail.com>
2023-04-09 19:23:39 -0300

    se agrega toString a rectangulo

--- src/figuras/rectangulo.java
+++ src/figuras/rectangulo.java
@@ -1,5 +1,21 @@
 package figuras;
 
 public class rectangulo {
+	private Double ancho;
+	private Double alto;
+	
+	public rectangulo(Double ancho, Double alto) {
+		this.ancho = ancho;
+	    this.alto = alto;
+	}
+	
+	public Double getPerimetro () {
+		return this.ancho*2 + this.alto*2;
+		
+	}
 
+	@Override
+	public String toString() {
+		return "rectangulo [ancho=" + ancho + ", alto=" + alto + "]";
+	}
 }
